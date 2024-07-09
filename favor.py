3aec3fbdd86c4e988a2bc5f30ac77da0a2dd34bc
Ariolucas <98797399+Ariolucas@users.noreply.github.com>
2024-07-09 10:52:26 +0000

    Tue, Jul 9, 2024, 12:52 PM +02:00

--- favor.py
+++ favor.py
@@ -3,17 +3,11 @@ import csv
 #prints the second column row[1] being programming language
 with open("program_names.csv") as file:
     reader  = csv.DictReader(file)
+    counts = {}
     php, ruby, python = 0, 0, 0,
     for row in reader:
         favor = row["Language"].strip().lower()
-        if favor == "python":
-            python += 1
-        elif favor == "php":
-            php += 1
-        elif favor == "ruby":
-            ruby += 1
-
-print(f"php:  {php}")
-print(f"python:  {python}")
-print(f"ruby: {ruby}")
-
+        if favor in counts:
+            counts[favor] += 1
+        else:
+            counts[favor] = 1
