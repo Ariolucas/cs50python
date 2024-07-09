ca223ace48e38f980117b36a75b7b222d68dc8fa
Ariolucas <98797399+Ariolucas@users.noreply.github.com>
2024-07-09 10:41:47 +0000

    Tue, Jul 9, 2024, 12:41 PM +02:00

--- fav.py
+++ fav.py
@@ -3,13 +3,15 @@ import csv
 #prints the second column row[1] being programming language
 with open("program_names.csv") as file:
     reader  = csv.DictReader(file)
-    PHP, Ruby, Python = 0, 0, 0
+    PHP, Ruby, Python = 0, 0, 0,
     for row in reader:
         favor = row["Language"]
-        if favor == "Python":
+        if favor == "python":
             Python += 1
         elif favor == "PHP":
-            
+            PHP += 1
+        elif favor == "Ruby":
+            Ruby += 1
 
 print(f"PHP:  {PHP}")
 print(f"Python:  {Python}")
